70c291ddddfa1d34734934661eeb991d9f2ff67b
ganirockz <ganeshrapeti8@gmail.com>
2020-10-03 01:14:04 +0530

    [Ganesh]Added:avoid duplicate entry of contact person details

--- src/com/capgemini/AddressBookMain.java
+++ src/com/capgemini/AddressBookMain.java
@@ -33,19 +33,18 @@ public class AddressBookMain {
 		int numberOfAddressBooks = Integer.parseInt(sc.nextLine());
 		for (int j = 0; j < numberOfAddressBooks; j++) {
 			String bookName;
-			while(true) {
-			System.out.println("Enter the name of the address book:");
-			bookName = sc.nextLine();
-			int flag = 0;
+			while (true) {
+				System.out.println("Enter the name of the address book:");
+				bookName = sc.nextLine();
+				int flag = 0;
 				for (Map.Entry<String, ArrayList<AddressBookMain>> entry : bookSystem.entrySet()) {
-					if(entry.getKey().equals(bookName)) {
+					if (entry.getKey().equals(bookName)) {
 						flag++;
 					}
 				}
-				if(flag == 0) {
+				if (flag == 0) {
 					break;
-				}
-				else {
+				} else {
 					System.out.println("already registerd with the name, try adding new book name");
 				}
 			}
@@ -53,27 +52,40 @@ public class AddressBookMain {
 			System.out.println("Enter the Number of persons you want to add in Address book: ");
 			int number = Integer.parseInt(sc.nextLine());
 			for (int i = 0; i < number; i++) {
-				System.out.println("Enter the contact details of person " + (i + 1) + ": ");
-				System.out.println("Enter the first name:");
-				String firstName = sc.nextLine();
-				System.out.println("Enter the last name:");
-				String lastName = sc.nextLine();
-				System.out.println("Enter the EMail:");
-				String Email = sc.nextLine();
-				System.out.println("Enter the Address:");
-				String Address = sc.nextLine();
-				System.out.println("Enter the City:");
-				String city = sc.nextLine();
-				System.out.println("Enter the State:");
-				String state = sc.nextLine();
-				System.out.println("Enter the zip:");
-				int zip = Integer.parseInt(sc.nextLine());
-
-				System.out.println("Enter the phone number:");
-				String phoneNumber = sc.nextLine();
-				AddressBookMain contact = new AddressBookMain(firstName, lastName, Email, Address, city, state, zip,
-						phoneNumber);
-				book.add(contact);
+				while (true) {
+					int flag = 0;
+					System.out.println("Enter the contact details of person " + (i + 1) + ": ");
+					System.out.println("Enter the first name:");
+					String firstName = sc.nextLine();
+					for(AddressBookMain c:book) {
+						if(c.FIRST_NAME.equals(firstName)) {
+							flag = 1;
+							break;
+						}
+					}
+					System.out.println("Enter the last name:");
+					String lastName = sc.nextLine();
+					System.out.println("Enter the EMail:");
+					String Email = sc.nextLine();
+					System.out.println("Enter the Address:");
+					String Address = sc.nextLine();
+					System.out.println("Enter the City:");
+					String city = sc.nextLine();
+					System.out.println("Enter the State:");
+					String state = sc.nextLine();
+					System.out.println("Enter the zip:");
+					int zip = Integer.parseInt(sc.nextLine());
+					System.out.println("Enter the phone number:");
+					String phoneNumber = sc.nextLine();
+					AddressBookMain contact = new AddressBookMain(firstName, lastName, Email, Address, city, state, zip,
+							phoneNumber);
+					if(flag == 0) {
+					book.add(contact);
+					break;}
+					else {
+						System.out.println("person with same name already exists. try another name");
+					}
+				}
 			}
 			bookSystem.put(bookName, book);
 		}
